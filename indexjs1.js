17317f329ec7813566f0a985f923c6e10bf5822a
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2018-02-22 23:36:01 +0530

    ssir

---
+++ indexjs1.js
@@ -0,0 +1,24 @@
+var pen = document.getElementById("pencilimg");
+
+$(document).ready(function(){
+    $(pen).click(function(){
+        $(pen).addClass("animated rollIn");
+    });
+});
+
+var pipe = document.getElementById("pipeimg");
+
+$(document).ready(function(){
+    $(pipe).click(function(){
+        $(pipe).addClass("animated rollIn");
+    });
+});
+
+var bucket = document.getElementById("bucketimg");
+
+$(document).ready(function(){
+    $(bucket).click(function(){
+        $(bucket).addClass("animated rollIn");
+    });
+});
+
